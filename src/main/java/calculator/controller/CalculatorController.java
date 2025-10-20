bb13c49e4074c8f947cd0ff632c7fee5d35c9e2c
khyej <wl159753@naver.com>
2025-10-20 16:50:25 +0900

    feat: 문자열 사용자 입력 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -14,5 +14,7 @@ public class CalculatorController {
 
     public void run() {
         view.displayInputPrompt();
+
+        String input = view.getInput();
     }
 }
--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -1,7 +1,13 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CalculatorView {
     public void displayInputPrompt() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public String getInput() {
+        return Console.readLine();
     }
 }
