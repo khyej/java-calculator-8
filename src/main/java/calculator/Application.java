02b86c81103f8bf599a8fd9322edc79cbd3d75b3
khyej <wl159753@naver.com>
2025-10-20 22:55:52 +0900

    fix: 프로그램 종료 시 Console 리소스 해제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,13 +3,18 @@ package calculator;
 import calculator.controller.CalculatorController;
 import calculator.model.StringCalculator;
 import calculator.view.CalculatorView;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        StringCalculator model = new StringCalculator();
-        CalculatorView view = new CalculatorView();
-        CalculatorController controller = new CalculatorController(model, view);
+        try {
+            StringCalculator model = new StringCalculator();
+            CalculatorView view = new CalculatorView();
+            CalculatorController controller = new CalculatorController(model, view);
 
-        controller.run();
+            controller.run();
+        } finally {
+            Console.close();
+        }
     }
 }
