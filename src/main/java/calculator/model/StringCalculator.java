9a3f5a7e0cbf4769a015ea3ec7ec83af26a5878e
khyej <wl159753@naver.com>
2025-10-20 17:04:51 +0900

    feat: 기본 구분자 문자열 분리

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,4 +1,14 @@
 package calculator.model;
 
 public class StringCalculator {
+    public int calculate(String text) {
+        String delimiter = ",|:";
+        String number = text;
+
+        return 0; // 임시 return
+    }
+
+    private String[] splitNumber(String delimiter, String number) {
+        return number.split(delimiter);
+    }
 }
